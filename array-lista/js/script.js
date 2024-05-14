473d9dad5037afb7518cd999dbd1081253c3f530
martinaliporata <martinaliporata@gmail.com>
2024-05-14 21:19:17 +0200

    added link from html to javascript

---
+++ array-lista/js/script.js
@@ -0,0 +1,19 @@
+// TuttiFrutti
+// Descrizione:
+// Abbiamo un frigorifero pieno di frutta: 'banana', 'mela', 'pera', 'ciliegia', 'arancia', 'mandarino', 'cocomero', 'limone', 'fragola'  
+// C'è anche una pesca sul tavolo, la mettiamo nel frigo.
+// Stasera dobbiamo fare un cocktail a base di cocomero: ce l'abbiamo o no in frigo?
+// Fasi
+// 1. creare l'array con la frutta del frigorifero
+// 2. aggiungere la pesca all'array della frutta
+// 3. verificare se nell'array di frutta c'è il cocomero:
+//    - se c'è stampiamo: "Trovato! Devo solo preparare il cocktail."
+//    - se non lo trovo: "Oh no, devo uscire a comprare il cocomero!"
+// Suggerimenti/Indicazioni:
+// Sì, lo sappiamo che esistono le funzioni includes() e indexOf() ma noi non le vogliamo utilizzare per cercare nel frigorifero.
+
+
+// Creiamo un array inserendo 'banana', 'mela', 'pera', 'ciliegia', 'arancia', 'mandarino', 'cocomero', 'limone', 'fragola'.
+// Aggiungiamo con .push 'pesca'
+// Usiamo find o findIndex per verificare se nell'array è presente il cocomero
+// Aggiungiamo un if per stampare una o l'altra situazione
---
+++ array_tail/js/script.js
@@ -0,0 +1,13 @@
+// Creiamo un array chiedendo all'utente quanti elementi dovrà contenere e
+// generando tanti numeri casuali (compresi nell'intervallo da 1 a 100) per quanti sono gli elementi da inserire.
+// Stampiamo poi gli ultimi 5 elementi dell'Array
+// Bonus:
+// Chiediamo all'utente quanti elementi dell'array dovremo stampare
+// Suggerimenti:
+// Probabilmente conviene controllare che il numero di elementi della coda non sia più grande del numero di elementi totali dell'array
+
+
+// Crea un prompt per chiedere all'utente quanti elementi vuole che l'array contenga
+// Crea un array con .split
+// Genera numeri con math.random da 1 a 100 con ciclo for (dopo < andrà il valore del prompt dell'utente)
+// Uso slice per stamapre gli utlimi cinque numeri dell'array
