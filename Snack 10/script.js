80e5a88a4a039664858bb3dc7ce82319658cd398
martinaliporata <martinaliporata@gmail.com>
2024-05-15 12:34:00 +0200

    correct

--- Snack 10/script.js
+++ Snack 10/script.js
@@ -1,9 +1,11 @@
 // Crea due array che hanno un numero di elementi diversi. 
 // Aggiungi elementi all'array che ha meno elementi fino a quando ne avrà tanti quanti l'altro.
 
-const arrayFirst = ['mela', 'mela','mela','mela','mela','mela','mela'];
-const arraySecond = ['mela','mela','mela','mela','mela','mela','mela','mela','mela','mela',];
+const arrayFirst = ['1', '2'];
+const arraySecond = ['1','2','3','4','5','6','7','8'];
 
 while (arrayFirst.length < arraySecond.length) {
-    arrayFirst.push('mela')
+    arrayFirst.push('1');
 }
+
+console.log(arrayFirst, arraySecond)
