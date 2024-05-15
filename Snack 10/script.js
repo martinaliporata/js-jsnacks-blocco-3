5778d5ea3816ed0012bb530067e04aa69388ad27
martinaliporata <martinaliporata@gmail.com>
2024-05-15 12:20:23 +0200

    added while

--- Snack 10/script.js
+++ Snack 10/script.js
@@ -1,7 +1,9 @@
 // Crea due array che hanno un numero di elementi diversi. 
 // Aggiungi elementi all'array che ha meno elementi fino a quando ne avrà tanti quanti l'altro.
 
-const arrayFirst = [1, 2, 3, 4, 5, 6, 7, 8, 10];
-const arraySecond = [1, 2, 3, 4, 5, 6, 7, 8, 10, 11, 12, 13, 14];
+const arrayFirst = ['mela', 'mela','mela','mela','mela','mela','mela'];
+const arraySecond = ['mela','mela','mela','mela','mela','mela','mela','mela','mela','mela',];
 
-while (arrayFirst.length === arraySecond.length)
+while (arrayFirst.length < arraySecond.length) {
+    arrayFirst.push('mela')
+}
