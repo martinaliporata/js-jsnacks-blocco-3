5e7575008baaff827a65c23d0b017631ce5c2a2e
martinaliporata <martinaliporata@gmail.com>
2024-05-15 12:11:35 +0200

    correct 2

--- Snack 9/script.js
+++ Snack 9/script.js
@@ -1,8 +1,7 @@
 // Crea un array vuoto. Chiedi fino a che l'array non ha 6 numeri all'utente di inserire un numero,
 //  se è dispari inseriscilo nell'array.
 
-const array []
-
+const array = [];
 
 while (array.length < 5) {
     let userNumber = Number.parseInt(prompt('Type a number'), 10);
