99ce43a26ee67fea404e482f06e6e967d6b72a61
martinaliporata <martinaliporata@gmail.com>
2024-05-15 11:58:25 +0200

    added array, while and if

---
+++ Snack 9/script.js
@@ -0,0 +1,10 @@
+// Crea un array vuoto. Chiedi fino a che l'array non ha 6 numeri all'utente di inserire un numero,
+//  se è dispari inseriscilo nell'array.
+
+const array []
+
+while (array <= 6) {
+    if (userNumber % 2 === 1) {
+        numbersArray.push(userNumber)
+    }
+}
