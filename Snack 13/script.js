f141a3bca14d6d3f2313c28ed6ee42f18d94aa00
martinaliporata <martinaliporata@gmail.com>
2024-05-16 12:48:39 +0200

    corretta

--- Snack 13/script.js
+++ Snack 13/script.js
@@ -6,3 +6,24 @@ function inversione(number) {
     let risultato = number.reverse;
     return risultato;
 }
+
+// correzione
+function getReversedNumber(number) {
+    // trasformo numero in una stringa
+    // un numero 92938939 + '' diventa una stringa
+    const string = number.toString();
+    let reversedString = '';
+
+    for (let index = string.lenght - 1;  index >= 0; index++) {
+        reversedString += string [index]
+    }
+
+    return Number.parseInt(reversedString,10);
+}
+
+const userNumber = Number.parseInt(prompt('type a number'), 10)
+const reversedString = getReversedNumber(userNumber)
+console.log(reversedString)
+
+
+// ascolta la registrazione dopo domanda di francesco bigazzi
