4103d08b73843774520d07b5f979d2c02281df6a
martinaliporata <martinaliporata@gmail.com>
2024-05-16 11:29:20 +0200

    added function with two arguments

---
+++ Snack 12/script.js
@@ -0,0 +1,11 @@
+// Dare la possibilità di inserire due parole. 
+// Verificare tramite una funzione che le due parole abbiano
+// la stessa lunghezza. 
+// Se hanno la stessa lunghezza, stamparle entrambe 
+// altrimenti stampare la più lunga delle due.
+
+function dueParole (parola1, parola2) {
+    
+}
+
+dueParole('Ciao', 'buongiorno')
