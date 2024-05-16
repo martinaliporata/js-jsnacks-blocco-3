5fa58715886730355d296501cb6aa2f1d8e749d0
martinaliporata <martinaliporata@gmail.com>
2024-05-16 11:35:50 +0200

    added ;

--- Snack 12/script.js
+++ Snack 12/script.js
@@ -4,15 +4,15 @@
 // Se hanno la stessa lunghezza, stamparle entrambe 
 // altrimenti stampare la più lunga delle due.
 
-const parola1 = prompt('dimmi una parola') 
-const parola2 = prompt('dimmi una seconda parola')
+const parola1 = prompt('dimmi una parola');
+const parola2 = prompt('dimmi una seconda parola');
 
 function dueParole (parola1, parola2) {
     if (parola1.lenght === parola2.lenght) {
         console.log(parola1, parola2)
     } else if (parola1.lenght < parola2.lenght) {
         console.log(parola2)
-    } else (parola1.lenght > parola2.lenght) {
+    } else (parola1.lenght > parola2.lenght); {
         console.log(parola1)
     }
 }
