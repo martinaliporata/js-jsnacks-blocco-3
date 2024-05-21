2e893d3b8579a0c23cbc4e45475592b3b641cf19
martinaliporata <martinaliporata@gmail.com>
2024-05-21 12:31:32 +0200

    inserted function for timer

--- Snack 14/script.js
+++ Snack 14/script.js
@@ -1,3 +1,9 @@
 // Chiedi all'utente un numero di secondi da attendere e fai partire un timer da quel momento a zero secondi, interrompendo il timer
 
 let userNumber = Number.parseInt(prompt('Scrivi quanti secondi attendere'))
+
+const timer = setInterval (function(){
+    if (userNumber < 0)
+        clearInterval(timer)
+        alert('Stop!')
+}, userNumber)
