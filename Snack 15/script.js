85a6af58faaa864c1a5d18c89d7aa82c4f9d283a
martinaliporata <martinaliporata@gmail.com>
2024-05-22 12:39:21 +0200

    added object

---
+++ Snack 15/script.js
@@ -0,0 +1,10 @@
+// Creare un oggetto palla che abbia le seguenti proprietà.
+// Nome = palla
+// Peso = 10
+// Attraverso un prompt dare la possibilità all'utente di modificare il peso della palla.
+
+const palla = {
+    "nome" : ['palla'],
+    "peso" : ['10'],
+}
+
