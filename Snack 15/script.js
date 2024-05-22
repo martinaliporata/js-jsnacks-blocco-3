9079a83c9d9325587ec7b1b13d8dd6d56d79e7b5
martinaliporata <martinaliporata@gmail.com>
2024-05-22 12:57:13 +0200

    correct

--- Snack 15/script.js
+++ Snack 15/script.js
@@ -7,6 +7,22 @@ const palla = {
     "nome" : ['palla'],
     "peso" : ['10'],
 }
+console.log(palla)
 
 const userChoice = prompt('Change balls weight')
+console.log(palla.peso = userChoice)
 
+
+// correzione
+// const palla = {
+//     nome : 'palla',
+//     peso : 10,
+// }
+
+// const userWord = Number.parseInt(prompt('Inserisci il peso da modificare'),10);
+// palla.peso = userWord;
+
+    // Altrimenti così
+    // palla.peso = Number.parseInt(prompt('Inserisci il peso da modificare'),10)
+
+// console.log(palla)
