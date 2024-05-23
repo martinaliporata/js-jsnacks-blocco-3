481d50acadb16754fc94a656ba529e5bc8ba1275
martinaliporata <martinaliporata@gmail.com>
2024-05-23 12:38:18 +0200

    created objects' array

---
+++ Snack objects 2/script.js
@@ -0,0 +1,57 @@
+// Crea un array di oggetti che rappresentano delle persone, 10.
+// Ogni persona ha un nome, un cognome e un'età.
+
+// Crea quindi un nuovo array inserendo, per ogni persona, una frase con il nome e cognome e l'indicazione se può guidare, in base all'età.
+
+const people = [
+    {
+        nome: 'lucia',
+        cognome: 'pippo',
+        età: 12
+    },
+    {
+        nome: 'paola',
+        cognome: 'seveso',
+        età: 15
+    },
+    {
+        nome: 'martina',
+        cognome: 'liporata',
+        età: 18
+    },
+    {
+        nome: 'marco',
+        cognome: 'minora',
+        età: 21
+    },
+    {
+        nome: 'luca',
+        cognome:'compagnoni',
+        età: 25
+    },
+    {
+        nome: 'alessandra',
+        cognome: 'terrioti',
+        età: 30
+    },
+    {
+        nome: 'diego',
+        cognome: 'bordegoni',
+        età: 10
+    },
+    {
+        nome: 'laura',
+        cognome: 'lampugnano',
+        età: 8
+    },
+    {
+        nome: 'lorenzo',
+        cognome: 'calzi',
+        età: 6,
+    },
+    {
+        nome: 'tiziano', 
+        cognome: 'amati',
+        età: 20,
+    },
+]
